c7331365db6fbd8fa8553bee2de7b3b196ee48e5
Gianluca Costa <gianluca@gianlucacosta.info>
2016-04-13 02:58:59 +0200

    Print OS information to the JavaScript console

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  var downloadButton = $("#download-program")
+  var downloadButton = $("#downloadProgram")
 
   var lowerCasePlatform = navigator.platform.toLowerCase()
 
@@ -11,15 +11,21 @@ $(document).ready(function() {
     var os = "mac"
   }
 
+  println("OS = " + os)
+
   var osFileRegex = new RegExp("-" + os + "\.(?:exe|zip)$" )
 
   $.get('https://api.github.com/repos/giancosta86/moondeploy/releases/latest', function (data) {
     var osFileUrl
 
     data.assets.forEach(function(asset) {
-      if (osFileRegex.test(asset.name)) {
+      var matchesOS = osFileRegex.test(asset.name)
+
+      if (matchesOS) {
         osFileUrl = asset.browser_download_url
       }
+
+      println(asset.name + " --> " + matchesOS)
     })
 
     if (osFileUrl) {
@@ -27,3 +33,10 @@ $(document).ready(function() {
     }
   });
 })
+
+
+function println(line) {
+  if (console && console.log) {
+    console.log(line)
+  }
+}
